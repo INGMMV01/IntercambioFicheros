366cf4d4525fbb7a5889262de289a3e1b69fb6a4
g000919 <g000919@externos.abanca.com>
2024-06-25 10:16:42 +0200

    Ata - Carga tipos de propiedad de petición

--- frontend/RPOSAA0415/src/app/peticiones/peticion-propiedades/nuevo/nuevo.component.ts
+++ frontend/RPOSAA0415/src/app/peticiones/peticion-propiedades/nuevo/nuevo.component.ts
@@ -3,11 +3,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IJsonApiData } from '@morphe/common';
+import { map, switchMap } from 'rxjs/operators';
 import { IEstadosPosiblesDeUnaPeticionResponseAttributes } from 'src/app/models/RPOS415/EstadosPosiblesDeUnaPeticionResponseAttributes';
+import { IPeticionResponseAttributes } from 'src/app/models/RPOS415/PeticionResponseAttributes';
 import { IPropiedadPeticionPostRequestAttributes } from 'src/app/models/RPOS415/PropiedadPeticionPostRequestAttributes';
+import { IPropiedadPeticionResponseAttributes } from 'src/app/models/RPOS415/PropiedadPeticionResponseAttributes';
 import { EstadoPosibleService } from 'src/app/services/estados/estado-posible.service';
+import { PeticionesService } from 'src/app/services/peticion/peticiones.service';
+import { PropiedadPeticionEstadoService } from 'src/app/services/propiedad-peticion/propiedad-peticion-estado.service';
 import { PropiedadPeticionService } from 'src/app/services/propiedad-peticion/propiedad-peticion.service';
-import { PropiedadesEstadoService } from 'src/app/services/propiedad-peticion/propiedades-estado.service';
 import { TipoPropiedadPeticionService } from 'src/app/services/propiedad-peticion/tipo-propiedad-peticion.service';
 
 @Component({
@@ -19,35 +23,40 @@ export class NuevoComponent {
 
     formulario: FormGroup;
     idPeticion = this.activatedRoute.snapshot.params['idPeticion'];
-    esPropiedadEstado = true;
-    tipoItemId = 'DLL'; // TODO: Cambiar por el valor correcto
+    esPropiedadEstado = false;
     estados: IJsonApiData<IEstadosPosiblesDeUnaPeticionResponseAttributes>[] = [];
     tiposDePropiedadDePeticion: any = []; // TODO: Cambiar por el valor correcto
     constructor(
         private activatedRoute: ActivatedRoute,
         private snackBar: MatSnackBar,
         private router: Router,
+        private peticionesService: PeticionesService,
         private propiedadPeticionService: PropiedadPeticionService,
+        private propiedadPeticionEstadoService: PropiedadPeticionEstadoService,
         private tipoPropiedadPeticionService: TipoPropiedadPeticionService,
         private estadoPosibleService: EstadoPosibleService) {
 
         this.formulario = new FormGroup({
             clave: new FormControl('', Validators.required),
-            nombre: new FormControl('', Validators.required),
             valor: new FormControl('', Validators.required),
         });
 
-        this.estadoPosibleService.get$(this.tipoItemId).subscribe(
-            (estados) => {
-                this.estados = estados;
+        this.peticionesService.getEntity$(this.idPeticion).pipe(
+            switchMap((peticion) => this.estadoPosibleService.get$(peticion.tipoItem)),
+            map(estados => estados.sort((a, b) => a.attributes.descripcion.localeCompare(b.attributes.descripcion)))
+        ).subscribe(
+            (estadosOrdenados) => {
+                this.estados = estadosOrdenados;
             }
         );
 
         this.tipoPropiedadPeticionService.get$().subscribe(
             (tipoPropiedades) => {
-                this.tiposDePropiedadDePeticion = tipoPropiedades; // establecer tipos de propiedades
+                this.tiposDePropiedadDePeticion = tipoPropiedades.sort((a, b) =>
+                    a.attributes.descripcion.localeCompare(b.attributes.descripcion));
             }
         );
+
     }
 
     public get cargando(): boolean {
@@ -60,6 +69,14 @@ export class NuevoComponent {
         const cargando: boolean = this.estadoPosibleService.cargando;
 
         return cargando;
+    }
+
+    changeTipoDePropiedad(valor: string): void {
+        if (valor) {
+            const propiedadPeticionResponseAttributes = new Object() as IPropiedadPeticionResponseAttributes;
+            propiedadPeticionResponseAttributes.clave = valor;
+            this.esPropiedadEstado = this.propiedadPeticionEstadoService.esPropiedadEstado(propiedadPeticionResponseAttributes);
+        }
     }
 
     guardar(): void {
